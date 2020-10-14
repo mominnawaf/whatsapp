dcec012eb93296600a7c17724395844a34d1fa37
mominnawaf <47545933+mominnawaf@users.noreply.github.com>
2020-10-14 22:22:17 +0530

    messageing added

--- src/components/SidebarChat.js
+++ src/components/SidebarChat.js
@@ -3,7 +3,6 @@ import React, {useEffect,useState} from 'react'
 import db from '../firbase';
 import './SidebarChat.css'
 import {Link} from 'react-router-dom'
-import userEvent from '@testing-library/user-event';
 
 function createChat(e){
 const name = prompt("Add Room");
@@ -15,24 +14,22 @@ if(name){
 }
 function SidebarChat({id,name,addNewChat}) {
     const [seed, setseed] = useState('')
-     const [messages,setMessages]=useState([]);
-        useEffect(()=>{
-       
-        if(id){
-        db.collection('Room').doc(id).collection('messages').orderBy
-        ('timestamp','desc').onSnapshot(snapshot =>(
-            setMessages(snapshot.docs.map((doc) => doc.data()))
-        ))
-        }
-        console.log(id)
-    },[])
+    //  const [messages,setMessages]=useState("");
+    //     useEffect(()=>{
+    //     if(id){
+    //     db.collection('Room').doc(id).collection('messages').orderBy
+    //     ('timestamp','desc').onSnapshot(snapshot =>(
+    //         setMessages(snapshot.docs.map((doc) => doc.data()))
+    //     ))
+    //     }
+    // },[])
     return !addNewChat? (
         <Link to={`/room/${id}`}>
         <div className="SidebarChat">
             <Avatar />
             <div className='SidebarInfo'>
                 <h2>{name}</h2>
-                <p>{messages[0]?.messages}</p>
+                {/*<p>{messages[0]?.messages}</p>*/}
             </div>
             
         </div>
